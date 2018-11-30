7e53f718d28a7487e6d70ae855d8f749e6d83c49
NamuhEvil <carlosstarw@gmail.com>
2018-11-30 16:14:53 +0800

    windows
    GUI Control

---
+++ Windows.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as tp
+import tkinter
+
+    
+if __name__ == '__main__':
+    #windows set
+    window = tkinter.Tk()
+    window.title('BackGround Remover')
+    window.geometry('500x500')
+    
+    # to generate the windows
+    window.mainloop()
+
+
+
