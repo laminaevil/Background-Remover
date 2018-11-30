9dbd0a4013a7194ecf4d7c04f14074f23557f53e
namuhevil <carlosstarw@gmail.com>
2018-11-30 16:28:08 +0800

    update to windows

--- Windows.py
+++ Windows.py
@@ -16,6 +16,12 @@ if __name__ == '__main__':
     
     # to generate the windows
     window.mainloop()
-
+    l = window.Label(window, 
+    text='OMG! this is TK!',    # 标签的文字
+    bg='green',     # 背景颜色
+    font=('Arial', 12),     # 字体和字体大小
+    width=15, height=2  # 标签长宽
+    )
+    l.pack()    # 固定窗口位置
 
 
