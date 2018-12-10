2aff5929dc84cdc93efa3d5536600a9e049eeab9
lonsuky <hpisj322@gmail.com>
2018-12-10 09:24:13 +0800

    update to training

--- Training Stage/train.py
+++ Training Stage/train.py
@@ -1,1 +1,17 @@
-# there are so many things to do here by training image
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from model import unet
+from time import time
+
+# load data
+dataset = h5py.File('dataset.h5', 'r')
+X_pre = dataset['X_pre'][:]
+Y_pre = dataset['Y_pre'][:]
+dataset.close()
+
+X_train = X_pre.astype('float32')
+X_train = X_train / 255.0
+
+
+
